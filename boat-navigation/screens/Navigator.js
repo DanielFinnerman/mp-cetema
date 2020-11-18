fd9baa8a3108d7825ed92579ae5fbd85c9d5b64b
Ville Liekari <57899741+villelie@users.noreply.github.com>
2020-11-18 12:44:21 +0200

    Update Navigator.js

--- boat-navigation/screens/Navigator.js
+++ boat-navigation/screens/Navigator.js
@@ -1,24 +1,38 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Icon } from 'native-base';
 
-import MainScreen from './MainScreen';
-import InfoScreen from './InfoScreen';
-import SettingsScreen from './SettingsScreen';
-import NauticalScreen from './NauticalScreen';
-import NauticalDetails from './NauticalScreenSingle';
+import LoginScreen from '../screens/LoginScreen';
+import MainScreen from '../screens/MainScreen';
+import InfoScreen from '../screens/InfoScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import NauticalScreen from '../screens/NauticalScreen';
+import NauticalDetails from '../screens/NauticalScreenSingle';
 
-import { colors } from '../helpers/globalVariables';
+import { colors } from '../variables/GlobalVariables';
+
+const AuthStack = createStackNavigator();
+
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator screenOptions={{
+      headerStyle: { backgroundColor: colors.dark.primary },
+      headerTintColor: colors.dark.tint
+    }}>
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+    </AuthStack.Navigator>
+  );
+}
 
 const InfoStack = createStackNavigator();
 
 const InfoStackScreen = () => {
   return (
     <InfoStack.Navigator screenOptions={{
-      headerStyle: { backgroundColor: colors.darkPrimary },
-      headerTintColor: colors.darkTint
+      headerStyle: { backgroundColor: colors.dark.primary },
+      headerTintColor: colors.dark.tint
     }}>
       <InfoStack.Screen name="Info" component={InfoScreen} />
       <InfoStack.Screen name="Weather" component={InfoScreen} />
@@ -33,8 +47,8 @@ const SettingsStack = createStackNavigator();
 const SettingsStackScreen = () => {
   return (
     <SettingsStack.Navigator screenOptions={{
-      headerStyle: { backgroundColor: colors.darkPrimary },
-      headerTintColor: colors.darkTint
+      headerStyle: { backgroundColor: colors.dark.primary },
+      headerTintColor: colors.dark.tint
     }}>
       <SettingsStack.Screen name="Settings" component={SettingsScreen} />
       <SettingsStack.Screen name="Theme" component={SettingsScreen} />
@@ -45,25 +59,32 @@ const SettingsStackScreen = () => {
 
 const Tab = createBottomTabNavigator();
 
+const TabNavigatorScreen = () => {
+  return (
+    <Tab.Navigator tabBarOptions={{
+      activeTintColor: colors.dark.accent,
+      activeBackgroundColor: colors.dark.secondary,
+      inactiveTintColor: colors.dark.tint,
+      inactiveBackgroundColor: colors.dark.primary,
+    }}>
+      <Tab.Screen name="Map" component={MainScreen} options={{
+        tabBarIcon: () => <Icon name='md-boat' style={{ color: colors.dark.tint }} />
+      }} />
+      <Tab.Screen name="Info" component={InfoStackScreen} options={{
+        tabBarIcon: () => <Icon name="md-cloudy" style={{ color: colors.dark.tint }} />
+      }} />
+      <Tab.Screen name="Settings" component={SettingsStackScreen} options={{
+        tabBarIcon: () => <Icon name="md-menu" style={{ color: colors.dark.tint }} />
+      }} />
+    </Tab.Navigator>
+  );
+}
+
 const Navigation = () => {
+  const isSigned = false;
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBarOptions={{
-        activeTintColor: colors.accent,
-        activeBackgroundColor: colors.darkSecondary,
-        inactiveBackgroundColor: colors.darkPrimary,
-        inactiveTintColor: colors.darkTint,
-      }}>
-        <Tab.Screen name="Map" component={MainScreen} options={{
-          tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name="map" color={tintColor} size={32} />
-        }} />
-        <Tab.Screen name="Info" component={InfoStackScreen} options={{
-          tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name="weather-cloudy" color={tintColor} size={32} />
-        }} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} options={{
-          tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name="settings" color={tintColor} size={32} />
-        }} />
-      </Tab.Navigator>
+      {isSigned ? TabNavigatorScreen() : AuthStackScreen()}
     </NavigationContainer>
   );
 }
